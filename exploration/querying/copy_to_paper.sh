dfbd7ebd2dedffa69b9916b213c5c09a56cbde92
Benedikt Kantz <benedikt@kantz.at>
2025-06-17 09:06:48 +0200

    redo plots with Qwen2.5

---
+++ exploration/querying/copy_to_paper.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+target_dir="$HOME/tugraz/cgv/papers/ickg-onset/generated"
+target_dir_figures="$target_dir/figures"
+target_dir_tables="$target_dir/tables"
+mkdir -p $target_dir
+mkdir -p $target_dir_figures
+mkdir -p $target_dir_tables
+cp -r figures/ $target_dir_figures
+cp -r tables/ $target_dir_tables
